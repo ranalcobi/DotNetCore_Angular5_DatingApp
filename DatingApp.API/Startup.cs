8f5f407d19da3b56b6681df23f9169a022a7bff5
Ran Alcobi <ran@ngworkshop.co.il>
2018-01-11 13:23:17 +0200

    Adding bootstrap and font-awesome

--- DatingApp.API/Startup.cs
+++ DatingApp.API/Startup.cs
@@ -27,6 +27,7 @@ namespace DatingApp.API
         {
             services.AddDbContext<DataContext>(b => b.UseSqlite(Configuration.GetConnectionString("DefaultConnection")));
             services.AddMvc();
+            services.AddCors();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -36,8 +37,8 @@ namespace DatingApp.API
             {
                 app.UseDeveloperExceptionPage();
             }
-
-            app.UseMvc();
+            app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().AllowAnyOrigin().AllowCredentials());
+            app.UseMvc(); // need to be last!
         }
     }
 }
